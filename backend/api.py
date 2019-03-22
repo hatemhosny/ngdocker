8e972bd97dce3ecc954c915745b41b601967bb87
Hatem Hosny <hatemhosny@gmail.com>
2019-03-23 01:13:50 +0200

    create backend

---
+++ backend/api.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class Products(Resource):
+    def get(self):
+        return {
+            'products': ['Ice cream',
+                        'Chocolate',
+                        'fruit'
+                        ]
+        }
+
+api.add_resource(Products, '/')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
+    
