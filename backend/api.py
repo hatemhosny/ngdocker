673d0a5bcc78a66aaaba05e30972beebe1e37ae2
Hatem Hosny <hatemhosny@gmail.com>
2019-03-24 23:53:15 +0200

    docker works!

--- backend/api.py
+++ backend/api.py
@@ -1,20 +1,23 @@
+#!/usr/bin/python
+
 from flask import Flask
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
+
 class Products(Resource):
     def get(self):
         return {
             'products': ['Ice cream',
-                        'Chocolate',
-                        'fruit'
-                        ]
+                         'Chocolate',
+                         'fruit'
+                         ]
         }
+
 
 api.add_resource(Products, '/')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
-    
+    app.run(host='0.0.0.0', port=80, use_reloader=False)
